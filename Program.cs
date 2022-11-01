f19d623a798ad3ed91ccf11833f78b5ba7ebe9b2
xingzhao1989 <62924053+xingzhao1989@users.noreply.github.com>
2022-11-01 22:50:50 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Restaurang
+{
+    public static void Main()
+    {
+        Console.Write("YOUR NAME!");
+        String name = Console.ReadLine();
+        Console.Write("YOUR SANLARY!");
+        String salary = Console.ReadLine();
+        Console.WriteLine("INFORMATION");
+        Console.WriteLine("NAME" + name);
+        Console.WriteLine("SALARY" + salary);
+
+    }
+}
+
+
+
